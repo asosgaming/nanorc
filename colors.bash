b1bb54d7b26b9d394e543f57e7b51192eaa8288e
mmensonen <marcomensonen@gmail.com>
2020-02-10 13:58:06 -0500

    Update colors.bash

--- colors.bash
+++ colors.bash
@@ -95,12 +95,41 @@ if [ -n "\$force_color_prompt" ]; then
 fi
 
 if [ "\$color_prompt" = yes ]; then
-    if [ "\$(hostname -s)" == "controller" ]; then
-        PS1='\[\033[00;36m\]\u\[\033[00;90m\]@\[\033[00;36m\]\h\[\033[01;37m\] \[\033[01;37m\][\[\033[01;34m\]\w\[\033[01;37m\]]\[\033[00;36m\]\\$\[\033[00m\] '
-    else
-        PS1='\[\033[00;32m\]\u\[\033[00;90m\]@\[\033[00;32m\]\h\[\033[01;37m\] \[\033[01;37m\][\[\033[01;34m\]\w\[\033[01;37m\]]\[\033[00;32m\]\\$\[\033[00m\] '
-    fi
-    declare -x LS_COLORS='di=1;40;34:ln=1;40;36:so=4;40;35:pi=4;40;33:ex=1;40;32:bd=1;4;40;31:cd=1;4;40;33:su=37;41:sg=30;43:tw=30;42:ow=34;42:rs=0:mh=00:do=1;4;35:or=1;5;36;40:mi=01;05;31;40:ca=30;41:st=37;44:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=01;36:*.au=01;36:*.flac=01;36:*.mid=01;36:*.midi=01;36:*.mka=01;36:*.mp3=01;36:*.mpc=01;36:*.ogg=01;36:*.ra=01;36:*.wav=01;36:*.axa=01;36:*.oga=01;36:*.spx=01;36:*.xspf=01;36:*.conf=1;93:*.cnf=1;93:*.cfg=1;93:*.yaml=1;93:*.yml=1;93:*.toml=1;93'
+   declare -x DCOLOR="[39m"
+   declare -x BLACK="[30m"
+   declare -x RED="[31m"
+   declare -x GREEN="[32m"
+   declare -x YELLOW="[33m"
+   declare -x BLUE="[34m"
+   declare -x MAGENTA="[35m"
+   declare -x CYAN="[36m"
+   declare -x LGRAY="[37m"
+   declare -x LGREY="[37m"
+   declare -x DGRAY="[90m"
+   declare -x DGREY="[90m"
+   declare -x LRED="[91m"
+   declare -x LGREEN="[92m"
+   declare -x LYELLOW="[93m"
+   declare -x LBLUE="[94m"
+   declare -x LMAGENTA="[95m"
+   declare -x LCYAN="[96m"
+   declare -x WHITE="[97m"
+
+   if [ "\$(hostname -s)" == "controller" ]; then
+        color="\$CYAN"
+   else
+        color="\$GREEN"
+   fi
+
+   host=\$(hostname -s)
+   dash="\033\${LGREEN}-\033\${color}"
+   dot="\033\${LGREEN}.\033\${color}"
+   host="\${host//-/\$dash}"
+   host="\${host//./\$dot}"
+
+   PS1="\033\${color}\u\033\${DGRAY}@\033\${color}\${host} \033\${WHITE}[\033\${LBLUE}\w\033\${WHITE}]\033\${color}\\$ \033${DCOLOR}"
+
+   declare -x LS_COLORS='di=1;40;34:ln=1;40;36:so=4;40;35:pi=4;40;33:ex=1;40;32:bd=1;4;40;31:cd=1;4;40;33:su=37;41:sg=30;43:tw=30;42:ow=34;42:rs=0:mh=00:do=1;4;35:or=1;5;36;40:mi=01;05;31;40:ca=30;41:st=37;44:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=01;36:*.au=01;36:*.flac=01;36:*.mid=01;36:*.midi=01;36:*.mka=01;36:*.mp3=01;36:*.mpc=01;36:*.ogg=01;36:*.ra=01;36:*.wav=01;36:*.axa=01;36:*.oga=01;36:*.spx=01;36:*.xspf=01;36:*.conf=1;93:*.cnf=1;93:*.cfg=1;93:*.yaml=1;93:*.yml=1;93:*.toml=1;93'
 else
     PS1='\${debian_chroot:+(\$debian_chroot)}\u@\h:\w\\$ '
 fi
@@ -134,7 +163,6 @@ fi
 alias ll='ls -alF -h --color'
 alias la='ls -A -h --color'
 alias l='ls -CF -h --color'
-alias maas='maas asos'
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
@@ -274,12 +302,41 @@ if [ -n "\$force_color_prompt" ]; then
 fi
 
 if [ "\$color_prompt" = yes ]; then
-    if [ "\$(hostname -s)" == "controller" ]; then
-        PS1='\[\033[00;35m\]\u\[\033[00;90m\]@\[\033[00;35m\]\h\[\033[01;37m\] \[\033[01;37m\][\[\033[01;34m\]\w\[\033[01;37m\]]\[\033[00;35m\]#\[\033[00m\] '
-    else
-        PS1='\[\033[00;31m\]\u\[\033[00;90m\]@\[\033[00;31m\]\h\[\033[01;37m\] \[\033[01;37m\][\[\033[01;34m\]\w\[\033[01;37m\]]\[\033[00;31m\]#\[\033[00m\] '
-    fi
-    declare -x LS_COLORS='di=1;40;34:ln=1;40;36:so=4;40;35:pi=4;40;33:ex=1;40;32:bd=1;4;40;31:cd=1;4;40;33:su=37;41:sg=30;43:tw=30;42:ow=34;42:rs=0:mh=00:do=1;4;35:or=1;5;36;40:mi=01;05;31;40:ca=30;41:st=37;44:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=01;36:*.au=01;36:*.flac=01;36:*.mid=01;36:*.midi=01;36:*.mka=01;36:*.mp3=01;36:*.mpc=01;36:*.ogg=01;36:*.ra=01;36:*.wav=01;36:*.axa=01;36:*.oga=01;36:*.spx=01;36:*.xspf=01;36:*.conf=1;93:*.cnf=1;93:*.cfg=1;93:*.yaml=1;93:*.yml=1;93:*.toml=1;93'
+   declare -x DCOLOR="[39m"
+   declare -x BLACK="[30m"
+   declare -x RED="[31m"
+   declare -x GREEN="[32m"
+   declare -x YELLOW="[33m"
+   declare -x BLUE="[34m"
+   declare -x MAGENTA="[35m"
+   declare -x CYAN="[36m"
+   declare -x LGRAY="[37m"
+   declare -x LGREY="[37m"
+   declare -x DGRAY="[90m"
+   declare -x DGREY="[90m"
+   declare -x LRED="[91m"
+   declare -x LGREEN="[92m"
+   declare -x LYELLOW="[93m"
+   declare -x LBLUE="[94m"
+   declare -x LMAGENTA="[95m"
+   declare -x LCYAN="[96m"
+   declare -x WHITE="[97m"
+
+   if [ "\$(hostname -s)" == "controller" ]; then
+        color="\$MAGENTA"
+   else
+        color="\$RED"
+   fi
+
+   host=\$(hostname -s)
+   dash="\033\${LRED}-\033\${color}"
+   dot="\033\${LRED}.\033\${color}"
+   host="\${host//-/\$dash}"
+   host="\${host//./\$dot}"
+
+   PS1="\033\${color}\u\033\${DGRAY}@\033\${color}\${host} \033\${WHITE}[\033\${LBLUE}\w\033\${WHITE}]\033\${color}\\$ \033${DCOLOR}"
+
+   declare -x LS_COLORS='di=1;40;34:ln=1;40;36:so=4;40;35:pi=4;40;33:ex=1;40;32:bd=1;4;40;31:cd=1;4;40;33:su=37;41:sg=30;43:tw=30;42:ow=34;42:rs=0:mh=00:do=1;4;35:or=1;5;36;40:mi=01;05;31;40:ca=30;41:st=37;44:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=01;36:*.au=01;36:*.flac=01;36:*.mid=01;36:*.midi=01;36:*.mka=01;36:*.mp3=01;36:*.mpc=01;36:*.ogg=01;36:*.ra=01;36:*.wav=01;36:*.axa=01;36:*.oga=01;36:*.spx=01;36:*.xspf=01;36:*.conf=1;93:*.cnf=1;93:*.cfg=1;93:*.yaml=1;93:*.yml=1;93:*.toml=1;93'
 else
     PS1='\${debian_chroot:+(\$debian_chroot)}\u@\h:\w\\$ '
 fi
